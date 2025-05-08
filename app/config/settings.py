6ddd58e0f9070eb0603add286f538069c9eb3159
Anushka Eshan <eanushka91@gmail.com>
2025-05-08 12:28:08 +0530

    fix db connection error

--- app/config/settings.py
+++ app/config/settings.py
@@ -17,9 +17,9 @@ S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'mycvstore')
 # PostgreSQL connection details
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '5432')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME', 'resumes')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', '1234')
 
 # Database URL
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
